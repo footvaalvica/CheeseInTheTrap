1e58c1c3b040a9d0c11dcf66b337d45ec6af674a
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-05 22:52:11 +0100

    start level design and stairs implementation

---
+++ scripts/Stairs.gd
@@ -0,0 +1,9 @@
+class_name Stairs extends Node2D
+
+func add_stairs(body : Node2D) -> void :
+	var player = body as Player
+	player.add_stairs(self)
+
+func remove_stairs(body : Node2D) -> void :
+	var player = body as Player
+	player.remove_stairs(self)
