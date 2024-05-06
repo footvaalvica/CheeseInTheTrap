c10f0c1c899d377dfbb8786d1b9dc44647594c61
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-06 18:08:48 +0100

    add highscore saver

---
+++ scripts/highscore/HighScoreEntry.gd
@@ -0,0 +1,22 @@
+class_name HighScoreEntry
+
+var _name : String
+var _cheese : int
+var _time : float
+
+func _init(name : String, cheese : int, time : float) :
+	_name = name
+	_cheese = cheese
+	_time = time
+
+func compare_to(other : HighScoreEntry) :
+	return compare_to_values(other._cheese, other._time)
+		
+func compare_to_values(cheese : int, time : float) :
+	if _cheese == cheese :
+		return _time - time
+	else :
+		return _cheese - cheese
+
+func to_dictionary() -> Dictionary :
+	return {"name" : _name, "cheese" : _cheese, "time" : _time}
