96afe036bd5f33d1411647036c63753b9ce21168
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-11 22:54:28 +0100

    do climb and stunned animations

--- scripts/TrapPlacer.gd
+++ scripts/TrapPlacer.gd
@@ -7,11 +7,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	super._process(delta)
+	stairs(delta)
 	if Input.is_action_just_pressed("trap_action_%s" % player_id): 
 		spawn_trap()
 	if Input.is_action_just_pressed("special_%s" % player_id) :
 		pickup_trap()
+
+func _physics_process(delta):
+	movement(delta)
 
 func finished_climbing() -> bool : # Trap placer shouldn't have climb delay
 	return true
@@ -22,7 +25,13 @@ func spawn_trap() -> void :
 func pickup_trap() -> void :
 	GameManager.instance().pick_up_trap(position, _floor)
 
-func stairs() -> void :
+func stairs(delta) -> void :
+	if _climbing_phase == Climbing_Phase.Entering :
+		stairs_enter(delta)
+		return
+	elif _climbing_phase == Climbing_Phase.Exiting :
+		stairs_exit(delta)
+		return
 	if Input.is_action_just_pressed("move_up_%s" % player_id):
 		move_to_floor(_floor + 1)
 	elif Input.is_action_just_pressed("move_down_%s" % player_id):
