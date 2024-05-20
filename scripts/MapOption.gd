bb234c31754dc3253c574adffa97e320abfc0185
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-20 11:22:46 +0100

    improve UI on map selection

--- scripts/MapOption.gd
+++ scripts/MapOption.gd
@@ -4,14 +4,24 @@ class_name MapOption extends  Node
 @export var locked_image : CompressedTexture2D
 var button : TextureButton
 var title : RichTextLabel
+var cheese_needed : RichTextLabel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	button = $MarginContainer2/VBoxContainer/TextureButton
-	title = $RichTextLabel
-	if resource == null or resource.necessary_cheese > 0:
+	title = $Title
+	cheese_needed = $"Cheese Needed"
+	if resource == null :
 		title.text = "[center]Locked[/center]"
 		button.texture_normal = locked_image
+		cheese_needed.text = "[center]Soon ![/center]"
+		return
+	if resource.necessary_cheese > 0 :
+		title.text = "[center]Locked[/center]"
+		button.texture_normal = locked_image
+		var cheese_diff = resource.necessary_cheese - 0
+		if cheese_diff > 0:
+			cheese_needed.text = "[center]%s cheese left to unlock[/center]" % cheese_diff
 		return
 	button.texture_normal = resource.map_image
 	title.text = "[center]%s[/center]" % resource.title
--- scripts/MapSelection.gd
+++ scripts/MapSelection.gd
@@ -1,11 +1,15 @@
 class_name MapSelection extends Node
 
 @export var total_cheese : int = 0
+@export var cheese_label : RichTextLabel = null
 var current_map_resource : MapResource
 static var instance : MapSelection
 
 func _init():
 	instance = self
+
+func _ready():
+	cheese_label.text = "[center]%s[/center]" % total_cheese
 
 func start() -> void :
 	get_tree().change_scene_to_packed(current_map_resource.map_scene)
