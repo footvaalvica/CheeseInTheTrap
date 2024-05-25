946fec99c331ed562ad691091e5f129cd095fe1e
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-25 13:04:38 +0100

    add missing UI navigation

--- scripts/highscore/HighScoreManager.gd
+++ scripts/highscore/HighScoreManager.gd
@@ -9,6 +9,7 @@ class_name HighScoreManager extends Node
 @export var score_label: Label
 @export var go_to_menu_button: Button
 @export var save_score_button: Button
+@export var go_back_button : Button
 @export var star_3: Sprite2D
 @export var star_2: Sprite2D
 @export var star_1: Sprite2D
@@ -21,6 +22,7 @@ var winner : String
 
 func _ready():
 	cheese = game_score.cheese
+	save_score_button.grab_focus()
 	time = game_score.time
 	winner = game_score.winner
 	score = (cheese * 3 - time) * 20
@@ -45,6 +47,7 @@ func add_score(name : String, cheese : int, time : float) :
 func _on_leaderboards_pressed():
 	await leaderboard.refresh_scores()
 	leaderboard_canvas_layer.show()
+	go_back_button.grab_focus()
 	canvas_layer.hide()
 
 func _on_save_score_pressed():
@@ -52,11 +55,13 @@ func _on_save_score_pressed():
 	add_score(name, cheese, time)
 	save_score_button.hide()
 	leaderboard_button.show()
+	leaderboard_button.grab_focus()
 	go_to_menu_button.show()
 
 func _on_button_pressed():
 	leaderboard_canvas_layer.hide()
 	canvas_layer.show()
+	leaderboard_button.grab_focus()
 
 func _on_go_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
