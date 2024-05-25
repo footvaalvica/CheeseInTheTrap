e27b2bb20594f1f071a53735ca0a173510e02b0d
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-25 17:54:13 +0100

    add jerry animations

--- scripts/interactables/shortcuts/GameShortcut.gd
+++ scripts/interactables/shortcuts/GameShortcut.gd
@@ -6,3 +6,13 @@ class_name GameShortcut extends Spammable
 func action():
 	# TODO : trigger animation of destruction ?
 	queue_free()
+
+func enter_traverse_area(body : Node2D) :
+	print_debug(body.name)
+	var jerry = body as Jerry
+	jerry.start_traverse_shortcut()
+
+func exit_traverse_area(body : Node2D):
+	print_debug(body.name)
+	var jerry = body as Jerry
+	jerry.stop_traverse_shortcut()
