d881faef4b38856c1f8dbe80de3c86b2576abb59
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-25 14:14:28 +0100

    add navigation to map selection

--- scripts/MapOption.gd
+++ scripts/MapOption.gd
@@ -1,31 +1,40 @@
-class_name MapOption extends  Node
+class_name MapOption extends  Control
 
 @export var resource : MapResource = null
 @export var locked_image : CompressedTexture2D
 var title : RichTextLabel
 var image : TextureRect
+var button : TextureButton
 var cheese_needed : RichTextLabel
+var cheese : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	title = $Title
 	image = $MarginContainer/VBoxContainer/TextureRect
+	button = $MarginContainer2/VBoxContainer/TextureButton
 	cheese_needed = $"Cheese Needed"
+	cheese = GameStateManager.cheese
+	focus_entered.connect(on_focus)
 	if resource == null :
 		title.text = "[center]Locked[/center]"
 		image.texture = locked_image
 		cheese_needed.text = "[center]Soon ![/center]"
 		return
-	if resource.necessary_cheese > 0 :
+	if resource.necessary_cheese > cheese :
 		title.text = "[center]Locked[/center]"
 		image.texture = locked_image
-		var cheese_diff = resource.necessary_cheese - 0
+		var cheese_diff = resource.necessary_cheese - cheese
 		if cheese_diff > 0:
 			cheese_needed.text = "[center]%s cheese left to unlock[/center]" % cheese_diff
 		return
 	image.texture = resource.map_image
 	title.text = "[center]%s[/center]" % resource.title
 
+func on_focus():
+	print_debug("focus")
+	button.grab_focus()
+	
 func select():
-	if resource != null :
+	if resource != null and resource.necessary_cheese <= GameStateManager.cheese:
 		MapSelection.instance.switch_map(resource)
--- scripts/MapSelection.gd
+++ scripts/MapSelection.gd
@@ -1,6 +1,8 @@
 class_name MapSelection extends Node
 
 @export var cheese_label : RichTextLabel = null
+@export var start_focus : Control 
+@export var start_button : Button
 var current_map_resource : MapResource
 var total_cheese : int = 0
 static var instance : MapSelection
@@ -11,6 +13,7 @@ func _init():
 	total_cheese = GameStateManager.cheese
 
 func _ready():
+	start_focus.grab_focus()
 	cheese_label.text = "[center]%s[/center]" % total_cheese
 
 func start() -> void :
@@ -23,5 +26,5 @@ func back() -> void :
 func switch_map(new_map : MapResource) -> void :
 	print_debug("switc")
 	current_map_resource = new_map
-
+	start_button.grab_focus()
 
