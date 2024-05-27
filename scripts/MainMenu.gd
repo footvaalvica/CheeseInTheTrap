38d572223b6e3957301eb885c4bbcf37605c4f18
Mateus Pinho <mateusleitepinho@gmail.com>
2024-05-27 14:53:49 +0100

    Revert "add quit functionality and basic sounds"
    This reverts commit 42fc68ce3194794dafbbb5fe322d69f9221efc69.

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -10,6 +10,9 @@ func _ready():
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/character_selection.tscn")
 
+func _on_quit_pressed():
+	get_tree().quit() 
+
 func _on_options_pressed():
 	options.show()
 	rest_of_menu.hide()
@@ -17,7 +20,3 @@ func _on_options_pressed():
 func _on_goback_pressed():
 	options.hide()
 	rest_of_menu.show()
-
-
-func _on_button_pressed():
-	get_tree().quit()
