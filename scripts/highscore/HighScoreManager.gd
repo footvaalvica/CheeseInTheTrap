cdddb8336a3a0696fce52030182c437ece20e594
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-30 00:17:50 +0100

    start local leaderoard

--- scripts/highscore/HighScoreManager.gd
+++ scripts/highscore/HighScoreManager.gd
@@ -5,7 +5,7 @@ class_name HighScoreManager extends Node
 @export var leaderboard_canvas_layer : CanvasLayer
 @export var canvas_layer : CanvasLayer
 @export var leaderboard_button: Button
-@export var leaderboard: LeaderboardUI
+@export var leaderboard : Leaderboard
 @export var score_label: Label
 @export var go_to_menu_button: Button
 @export var save_score_button: Button
@@ -14,6 +14,11 @@ class_name HighScoreManager extends Node
 @export var star_2: Sprite2D
 @export var star_1: Sprite2D
 @export var winner_text: RichTextLabel
+
+var high_score_map : Dictionary
+var high_score_table :  Array[HighScoreEntry]
+const MAX_TABLE = 10
+const FILE_PATH = "res://highscores.dat"
 
 var cheese : int
 var max_cheese : int
@@ -27,10 +32,9 @@ func _ready():
 	save_score_button.grab_focus()
 	time = game_score.time
 	winner = game_score.winner
-	score = (cheese * 3 - time) * 20
-	score = 100000
-	score_label.text = "Score:" + str(score)
+	score_label.text = "Time:" + str(time)
 	winner_text.text = "[rainbow][center]Winner: " + winner
+	load_highscores()
 	
 	if cheese > 0:
 		star_1.show()
@@ -38,15 +42,71 @@ func _ready():
 		star_2.show()
 	if cheese == max_cheese:
 		star_3.show()
-	
+
+func _exit_tree():
+	save_highscores()
+
 func add_score(name : String, cheese : int, time : float) :
-	var score: int = (cheese * 3 - time) * 20
-	var success: bool = await Leaderboards.post_guest_score("cheeseinthetrap-highscore-wHeL", score, name)
-	print_debug(success)
+	var entry : HighScoreEntry = HighScoreEntry.new(name, cheese, time)
+	var length : int = min(high_score_table.size() + 1, MAX_TABLE)
+	var i : int = 0
+	var propagate : bool = false
+	if high_score_table.size() < length :
+		high_score_table.resize(length)
+	while i < length:
+		if high_score_table[i] == null or entry.compare_to(high_score_table[i]) \
+			> 0 or propagate :
+			var tmp = high_score_table[i]
+			high_score_table[i] = entry
+			entry = tmp
+			propagate = true
+		i += 1
+	save_highscores()
+
+func is_new_score(cheese : int, time : float, map : String) :
+	var length = min(high_score_table.size() + 1, MAX_TABLE)
+	return high_score_table[length].compare_to_values(cheese, time)
+
+func load_highscores() : 
+	if not FileAccess.file_exists(FILE_PATH) :
+		var new_file = FileAccess.open(FILE_PATH, FileAccess.WRITE)
+		new_file.store_string("[\n]")
+		new_file.close()
+		return
+	var highscore_file = FileAccess.open(FILE_PATH, FileAccess.READ)
+	highscore_file.get_line() # first line is [
+	var line = highscore_file.get_line()
+	while line != "]" :
+		var content : Dictionary = JSON.parse_string(line)
+		var entry = HighScoreEntry.new(content["name"], content["cheese"], content["time"])
+		high_score_table.append(entry)
+		line = highscore_file.get_line()
+	highscore_file.close()
+
+func save_highscores() :
+	var file = FileAccess.open(FILE_PATH, FileAccess.WRITE)
+	file.store_line("{")
+	for i in range(high_score_map.keys().size()) :
+		var map_name = high_score_map.keys()[i]
+		file.store_string("\"%s\" : [" % map_name)
+		for j in high_score_table.size() :
+			var entry = high_score_table[j]
+			file.store_string(JSON.stringify(entry.to_dictionary()))
+			if j < high_score_table.size() - 1 :
+				file.store_string(",")
+			file.store_string("\n")
+		if (i < high_score_map.keys().size() - 1) :
+			file.store_line("\t],")
+		else :
+			file.store_line("\t]")
+	file.store_line("}")
+	file.close()
 
 func _on_leaderboards_pressed():
-	await leaderboard.refresh_scores()
 	leaderboard_canvas_layer.show()
+	leaderboard.scores = high_score_map
+	leaderboard.map_index = 0
+	leaderboard.refresh()
 	go_back_button.grab_focus()
 	canvas_layer.hide()
 
