60dedc594f1d1d0b63c4f8e0fa59f13c507a7e2c
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-05-31 22:14:06 +0100

    add interaction logs with folders

--- scripts/interactables/collectables/Cheese.gd
+++ scripts/interactables/collectables/Cheese.gd
@@ -4,4 +4,5 @@ class_name	Cheese extends Node2D
 
 func on_collision(body : Node2D) -> void:
 	GameManager.instance().collect_cheese()
+	InteractionLogManager.save_cheese_caught(name)
 	queue_free()
