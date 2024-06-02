48ab91495cd88f7f8c798270b5f1a1bbc081e6e3
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-06-02 21:45:06 +0100

    fix trap leave error

--- scripts/interactables/collectables/Trap.gd
+++ scripts/interactables/collectables/Trap.gd
@@ -86,5 +86,5 @@ func _on_area_2d_body_entered(body : Node2D):
 func _on_area_2d_body_exited(body):
 	$DisarmArea.hide()
 	var jerry = body as Jerry
-	_in_zone = jerry
+	_in_zone = null
 	jerry.remove_trap(self)
