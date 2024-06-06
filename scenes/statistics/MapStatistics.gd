64b204e23919310dc525e0337c13ff035b4b9ab6
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-06-06 23:09:57 +0100

    statistics

--- scenes/statistics/MapStatistics.gd
+++ scenes/statistics/MapStatistics.gd
@@ -1,11 +1,17 @@
 class_name MapStatistics extends Node
 	
-@export var heat : PackedScene
+@export var death_heat : PackedScene
+@export var trap_heat : PackedScene
 var entry : StatisticsEntry
 	
 func apply_death_heat():
-	print_debug("death : %d" % entry.death_positions.size())
 	for death :  Vector2 in entry.death_positions :
-		var instance : Node2D = heat.instantiate() as Node2D
+		var instance : Node2D = death_heat.instantiate() as Node2D
 		instance.position = death
+		get_parent().add_child(instance)
+
+func apply_trap_heat():
+	for trap : Vector2 in entry.trap_positions :
+		var instance : Node2D = trap_heat.instantiate() as Node2D
+		instance.position = trap
 		get_parent().add_child(instance)
